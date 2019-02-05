133bb429160b5e0b4806882394a5f8eea52b768f
melron <sebastien.avenel@outlook.fr>
2019-02-05 17:59:00 +0100

    Creating views 'register.twig' and 'forgotPassword.twig' in 'templates' folder

--- App/public/index.php
+++ App/public/index.php
@@ -23,6 +23,12 @@ switch ($page){
     case 'signin':
         echo $twig->render('signIn.twig');
         break;
+    case 'register':
+        echo $twig->render('register.twig');
+        break;
+    case 'forgotPassword':
+        echo $twig->render('forgotPassword.twig');
+        break;
     default:
         header('HTTP/1.0 404 Not Found');
         echo $twig->render('404.twig');
